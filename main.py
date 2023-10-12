5a3798d289d8fcab052c0856a89acafacae40c92
Alan <alancr612@gmail.com>
2023-10-12 03:41:26 -0600

    Cambiada la key a un archivo aparte y agregada una función de bienvenida y despedida

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
+# Importar las librerias de discord
 import discord
 from discord.ext import commands
 
-client = commands.Bot(command_prefix='$')
+# Importar el token de un archivo por separado
+from apikeys import Token
+
+intents = discord.Intents.all()
+client = commands.Bot(command_prefix = '$', intents=intents)
 
 @client.event
-async def inicio():
-    print("Listo para chambear")
+async def on_ready():
+    print("Bot inicializado")
     print("-------------------------")
 
 @client.command()
 async def hola(ctx):
     await ctx.send("Hola mundo")
 
-client.run('')
+@client.event
+async def on_member_join(miembro):
+    canal = client.get_channel(1161591714193096715)
+    await canal.send(f"Bienvenido/a {miembro.mention} :)")
+
+@client.event
+async def on_member_remove(miembro):
+    canal = client.get_channel(1161591714193096715)
+    await canal.send(f"Adios {miembro.mention}")
+
+client.run(Token())
