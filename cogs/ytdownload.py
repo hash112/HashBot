ccc83b088df569702d51d2b0b0380197c594518f
Alan <dev.alancr@gmail.com>
2023-12-29 19:55:57 -0600

    Empecé a agregar logs y removí librería Django para hacer el dashboard en un entorno aparte

--- cogs/ytdownload.py
+++ cogs/ytdownload.py
@@ -3,23 +3,36 @@ from nextcord.ext import commands
 
 from pytube import YouTube
 import os
+import datetime
 
 class Ytdownloader(commands.Cog):
+
     def __init__(self, client):
         self.client = client
 
     @commands.command()
     async def ytmp3(self, ctx, link): # Descarga y manda un video de youtube en formato audio mp3
-        yt = YouTube(link)
-        song = yt.streams.filter(only_audio=True, file_extension="mp4").last()
+        try:
+            yt = YouTube(link)
+            song = yt.streams.filter(only_audio=True, file_extension="mp4").last()
 
-        # Los videos se almacenan temporalmente hasta que se envían
-        out_file = song.download(output_path="./tests")
-        name, ext = os.path.splitext(out_file)
-        new_file = name + ".mp3"
-        os.rename(out_file, new_file)
-        await ctx.send(file=nextcord.File(new_file))
-        os.remove(new_file)
+            print(f"[{datetime.datetime.now()}] Valid link in server: {ctx.guild.name}")
+
+            # Los videos se almacenan temporalmente hasta que se envían
+            out_file = song.download(output_path="./tests")
+            name, ext = os.path.splitext(out_file)
+            new_file = name + ".mp3"
+            os.rename(out_file, new_file)
+            await ctx.send(file=nextcord.File(new_file))
+            os.remove(new_file)
+
+            print(f"[{datetime.datetime.now()}] File sent at chat: {ctx.channel.name} in server: {ctx.guild.name}")
+            return 0
+
+        except:
+            print(f"[{datetime.datetime.now()}] Link error in server: {ctx.guild.name}")
+            await ctx.send(f"Hubo un error en el link, intenta de nuevo")
+            return 1
 
 def setup(client):
     client.add_cog(Ytdownloader(client))
--- main.py
+++ main.py
@@ -6,13 +6,15 @@ import os
 # Importar el token de un archivo por separado
 from api_secret import BOTTOKEN
 
+import datetime
+
 intents = nextcord.Intents.all()
 client = commands.Bot(command_prefix = '$', intents=intents)
 
 @client.event
 async def on_ready():
-    print(f"Bot inicializado como {client.user}")
-    print("------------------------------------")
+    print("----------------------------[LOG]----------------------------")
+    print(f"[{datetime.datetime.now()}] Bot inicializado como {client.user}")
     return
 
 extensiones = [f"cogs.{filename[:-3]}" for filename in os.listdir('./cogs') if filename.endswith(".py")]
